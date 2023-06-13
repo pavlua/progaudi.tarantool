67e09d5b43e57b4c0fc7f373f306d90c28758a8f
Paul Grigorenko <paull@inbox.ru>
2023-06-13 19:04:09 +0300

    updated sample app from .netcoreapp2 to .net6

--- samples/docker-compose/dotnet/Program.cs
+++ samples/docker-compose/dotnet/Program.cs
@@ -1,25 +1,32 @@
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using dotnet.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using ProGaudi.MsgPack.Light;
+using ProGaudi.Tarantool.Client.Model;
+using ProGaudi.Tarantool.Client;
 
-namespace dotnet
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables("ASPNETCORE_")
-                .Build();
+var builder = WebApplication.CreateBuilder(args);
 
-            var host = WebHost
-                .CreateDefaultBuilder(args)
-                .UseConfiguration(config)
-                .UseStartup<Startup>()
-                .Build();
+// Add services to the container.
+builder.Services.AddMvc();
 
-            host.Run();
-        }
-    }
-}
+var msgPackContext = new MsgPackContext();
+msgPackContext.GenerateAndRegisterArrayConverter<Dog>();
+
+var clientOptions = new ClientOptions("operator:123123@tarantool1:3301", context: msgPackContext);
+
+var box = new Box(clientOptions);
+box.Connect().ConfigureAwait(false).GetAwaiter().GetResult();
+
+builder.Services.AddSingleton(box);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
